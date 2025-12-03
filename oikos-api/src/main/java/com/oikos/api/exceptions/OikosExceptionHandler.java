116300d9c84950d2fbd0ba73574c7198114fcc4e
Artur trombim <arturtrombim.dev@gmail.com>
2025-12-03 00:32:24 -0300

    feat: update OikosExceptionHandler to return ResponseEntity for exception handling

--- oikos-api/src/main/java/com/oikos/api/exceptions/OikosExceptionHandler.java
+++ oikos-api/src/main/java/com/oikos/api/exceptions/OikosExceptionHandler.java
@@ -1,16 +1,38 @@
 package com.oikos.api.exceptions;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class OikosExceptionHandler {
 
     @ExceptionHandler(OikosException.class)
-    public OikosErrorResponse handleOikosException(OikosException ex) {
-        return OikosErrorResponse.builder()
-                .codigo(ex.getCodigo())
-                .timestamp(java.time.LocalDateTime.now())
-                .build();
+    public ResponseEntity<OikosErrorResponse>  handleOikosException(OikosException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(OikosErrorResponse.builder()
+                        .codigo(ex.getCodigo())
+                        .timestamp(LocalDateTime.now())
+                        .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<OikosErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+
+        FieldError fieldError = ex.getBindingResult().getFieldErrors().get(0);
+
+        String codigoErro = fieldError.getDefaultMessage();
+
+        return ResponseEntity
+                .badRequest()
+                .body(OikosErrorResponse.builder()
+                        .codigo(codigoErro)
+                        .timestamp(LocalDateTime.now())
+                        .build());
     }
 }
